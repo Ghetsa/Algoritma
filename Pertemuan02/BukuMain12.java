125d2c6f9fe486dbdbd537fb51b029e33ebf5aac
Ghetsa <ghetsa.arryanti@gmail.com>
2024-02-19 19:59:35 +0700

    add: BukuMain12.java

---
+++ Pertemuan02/BukuMain12.java
@@ -0,0 +1,22 @@
+package Algoritma.Pertemuan02;
+
+public class BukuMain12 {
+    public static void main(String[] args) {
+        Buku12 bk1 = new Buku12();
+        bk1.judul = "Today Ends Tomorrow Comes";
+        bk1.pengarang = "Denanda Pratiwi";
+        bk1.halaman = 198;
+        bk1.stok = 13;
+        bk1.harga = 71000;
+
+        bk1.tampilInformasi();
+        bk1.terjual(5);
+        bk1.gantiHarga(60000);
+        bk1.tampilInformasi();
+
+        Buku12 bk2 = new Buku12("Self Reward", "Maheera Ayesha", 160, 29, 59000);
+        bk2.terjual(11);
+        bk2.tampilInformasi();
+
+    }
+}
