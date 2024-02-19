c5fad7c953d7422381ff1c11994dae7d0c090d6b
Ghetsa <ghetsa.arryanti@gmail.com>
2024-02-19 20:00:13 +0700

    add: DragonMain.java

---
+++ Pertemuan02/DragonMain.java
@@ -0,0 +1,48 @@
+package Algoritma.Pertemuan02;
+
+import java.util.Scanner;
+
+public class DragonMain {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan nilai awal X: ");
+        int x = input.nextInt();
+        System.out.print("Masukkan nilai awal Y: ");
+        int y = input.nextInt();
+        System.out.print("Masukkan lebar area: ");
+        int width = input.nextInt();
+        System.out.print("Masukkan tinggi area: ");
+        int height = input.nextInt();
+
+        Dragon koor = new Dragon();
+        koor.x = x;
+        koor.y = y;
+        koor.width = width;
+        koor.height = height;
+
+        while (x > 0 || y > 0 || x < width || y < height) {
+            System.out.print("Masukkan Perintah(L/R/U/D):");
+            char perintah = input.next().charAt(0);
+
+            switch (perintah) {
+                case 'L':
+                    koor.moveLeft();
+                    break;
+                case 'R':
+                    koor.moveRight();
+                    break;
+                case 'U':
+                    koor.moveUp();
+                    break;
+                case 'D':
+                    koor.moveDown();
+                    break;
+                default:
+                    break;
+            }
+
+            koor.printPosition();
+        }
+    }
+}
