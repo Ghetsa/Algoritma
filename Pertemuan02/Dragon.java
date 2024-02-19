0b9b250d17e1ed955ca358106bf77479ff78bdf2
Ghetsa <ghetsa.arryanti@gmail.com>
2024-02-19 19:59:55 +0700

    add: Dragon.java

---
+++ Pertemuan02/Dragon.java
@@ -0,0 +1,46 @@
+package Algoritma.Pertemuan02;
+
+public class Dragon {
+    int x, y, width, height;
+
+    void moveLeft() {
+        x -= 1;
+        if (x < 0) {
+            detectCollision();
+
+        }
+    }
+
+    void moveRight() {
+        x += 1;
+        if (x > width) {
+            detectCollision();
+        }
+    }
+
+    void moveUp() {
+        y -= 1;
+        if (y > height) {
+            detectCollision();
+        }
+    }
+
+    void moveDown() {
+        y += 1;
+        if (y < 0) {
+            detectCollision();
+        }
+    }
+
+    void printPosition() {
+        System.out.println("Posisi: " + x + "," + y);
+    }
+
+    void detectCollision() {
+
+        System.out.println("Game Over");
+
+        System.exit(0);
+    }
+
+}
