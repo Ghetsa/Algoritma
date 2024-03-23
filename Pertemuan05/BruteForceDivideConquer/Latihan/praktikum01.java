9552417c1f685ead6b00d27aa5f6ae5ce3e8c7ad
Ghetsa <ghetsa.arryanti@gmail.com>
2024-03-23 08:42:50 +0700

    update: praktikum01

--- Pertemuan05/BruteForceDivideConquer/Latihan/praktikum01.java
+++ Pertemuan05/BruteForceDivideConquer/Latihan/praktikum01.java
@@ -25,10 +25,19 @@ public class praktikum01 {
         return Math.min(lMin, rMin);
     }
 
-    static int FindIntegerInArray(int[] found, int foundWhat) {
+    private static double AveTopPower(int[] arr) {
+        int total = 0;
+        for (int i = 0; i < arr.length; i++) {
+            total += arr[i];
+        }
+        double average = (double) total / arr.length;
+        return average;
+    }
+
+    static int FindIntegerInArray(int[] cari, int cariapabang) {
         int hasil = 0;
-        for (int i = 0; i < found.length; i++) {
-            if (found[i] != 0 && found[i] == foundWhat) {
+        for (int i = 0; i < cari.length; i++) {
+            if (cari[i] != 0 && cari[i] == cariapabang) {
                 hasil = i;
                 break;
             } else {
@@ -44,19 +53,31 @@ public class praktikum01 {
                 "Golf GTI" };
         int[] tahun = { 2016, 2014, 2009, 2014, 2013, 1986, 2014, 2014 };
         int[] top_acc = { 6816, 3921, 4360, 4058, 6255, 3700, 4180, 4180 };
+        int[] top_power = { 728, 575, 657, 609, 703, 553, 609, 631 };
 
         int maxAcc = MaxTopACC(top_acc, 0, top_acc.length - 1);
         int minAcc = MinTopACC(top_acc, 0, top_acc.length - 1);
         int iMax = FindIntegerInArray(top_acc, maxAcc);
         int iMin = FindIntegerInArray(top_acc, minAcc);
+        double avePower = AveTopPower(top_power);
 
-        System.out.println("++++ TOP ACC TERTINGGI ++++");
-        System.out.println("Merk: " + merk[iMax]);
-        System.out.println("Tipe: " + merk[iMax]);
-        System.out.println("Tahun: " + merk[iMax]);
+        System.out.println("+++++++ TOP ACC TERTINGGI +++++++");
+        System.out.println("Merk     : " + merk[iMax]);
+        System.out.println("Tipe     : " + tipe[iMax]);
+        System.out.println("Tahun    : " + tahun[iMax]);
+        System.out.println("Top power: " + top_power[iMax]);
         System.out.println("Top acceleration tertinggi: " + maxAcc);
-
+        System.out.println("=================================");
+        System.out.println("------- TOP ACC TERENDAH -------");
+        System.out.println("Merk     : " + merk[iMin]);
+        System.out.println("Tipe     : " + tipe[iMin]);
+        System.out.println("Tahun    : " + tahun[iMin]);
+        System.out.println("Top power: " + top_power[iMin]);
         System.out.println("Top acceleration terendah: " + minAcc);
+        System.out.println("=================================");
+        System.out.println("------ RATA-RATA TOP POWER ------");
+        System.out.println("Rerata top power: " + avePower);
+        System.out.println("=================================");
     }
 
 }
