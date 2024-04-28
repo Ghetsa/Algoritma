49553c9489c72fe802a2933a7d704ca1f05dc2b4
Ghetsa <ghetsa.arryanti@gmail.com>
2024-04-28 21:04:11 +0700

    update: latihan praktikum

--- Pertemuan08/Percobaan/Percobaan1_2/Gudang12.java
+++ Pertemuan08/Percobaan/Percobaan1_2/Gudang12.java
@@ -29,12 +29,13 @@ public class Gudang12 {
     }
 
     public void tambahBarang(Barang12 brg) {
+        System.out.println("----------------------------------------------");
         if (!cekPenuh()) {
             top++;
             tumpukan[top] = brg;
-            System.out.println("Barang " + brg.nama + " berhasil ditambahkan ke Gudang");
+            System.out.printf("|%-43s %s|%n", " [" + brg.nama + "] berhasil ditambah ke Gudang", "");
         } else {
-            System.out.println("Gagal! Tumpukan barang di Gudang sudah Penuh!!");
+            System.out.printf("|%-43s %s|%n", " Gagal! Tumpukan barang sudah penuh!!", "");
         }
         System.out.println("----------------------------------------------");
 
@@ -44,10 +45,12 @@ public class Gudang12 {
         if (!cekKosong()) {
             Barang12 delete = tumpukan[top];
             top--;
+            System.out.println("==============================================");
+            System.out.printf("|%-43s %s|%n", " [" + delete.nama + "] berhasil diambil ke Gudang", "");
             System.out.println("----------------------------------------------");
-            System.out.printf("|%-43s %s|%n", " Barang [" + delete.nama + "] berhasil diambil ke Gudang", "");
-            System.out.printf("|%-27s| %-15s|%n", " Kode unik dalam biner", konversiDesimalKeBiner(delete.kode));
-            System.out.println("----------------------------------------------");
+            System.out.printf("|%-27s| %-15s|%n", " Kode unik dalam biner",
+                    konversiDesimalKeBiner(delete.kode));
+            System.out.println("==============================================");
 
         } else {
             System.out.println("Gagal! Tumpukan barang di Gudang Kosong!!");
@@ -55,32 +58,43 @@ public class Gudang12 {
     }
 
     public Barang12 lihatBarangTeratas() {
+        System.out.println("==============================================");
+        System.out.println("|                BARANG TERATAS              |");
+        System.out.println("----------------------------------------------");
         if (!cekKosong()) {
             Barang12 barangTeratas = tumpukan[top];
-            System.out.println("Barang teratas: " + barangTeratas.nama);
+            System.out.printf("|%-43s %s|%n", " Kode       | " + barangTeratas.kode, "");
+            System.out.printf("|%-43s %s|%n", " Nama       | " + barangTeratas.nama, "");
+            System.out.printf("|%-43s %s|%n", " Kategori   | " + barangTeratas.kategori, "");
+            System.out.println("==============================================");
             return barangTeratas;
         } else {
-            System.out.println("Tumpukan barang kosong");
+            System.out.println("         !!! TUMPUKAN BARANG KOSONG !!!       ");
+            System.out.println("==============================================");
             return null;
         }
+
     }
 
     public void tampilkanBarang() {
+        System.out.println("==============================================");
+        System.out.println("|            RINCIAN TUMPUKAN BARANG         |");
+        System.out.println("----------------------------------------------");
         if (!cekKosong()) {
-            System.out.println("Rincian tumpukan barang di Gudang");
             for (int i = 0; i <= top; i++) {
-                System.out.printf("Kode %d: %s (Kategori %s)\n", tumpukan[i].kode, tumpukan[i].nama,
+                System.out.printf("| Kode %-3d| Nama %-9s| Kategori %-8s|\n", tumpukan[i].kode, tumpukan[i].nama,
                         tumpukan[i].kategori);
             }
         } else {
-            System.out.println("Tumpukan Barang Kosong");
+            System.out.println("         !!! TUMPUKAN BARANG KOSONG !!!       ");
         }
+        System.out.println("==============================================");
     }
 
     // percobaan 2
     public String konversiDesimalKeBiner(int kode) {
         StackKonversi stack = new StackKonversi();
-        while (kode > 0) {
+        while (kode != 0) {
             int sisa = kode % 2;
             stack.push(sisa);
             kode = kode / 2;
@@ -92,5 +106,119 @@ public class Gudang12 {
         }
         return biner;
 
+    }
+
+    // Praktikum
+    public Barang12 lihatBarangTerbawah() {
+        System.out.println("==============================================");
+        System.out.println("|                BARANG TERBAWAH              |");
+        System.out.println("----------------------------------------------");
+        if (!cekKosong()) {
+            Barang12 barangTerbawah = tumpukan[0];
+            System.out.printf("|%-43s %s|%n", " Kode       | " + barangTerbawah.kode, "");
+            System.out.printf("|%-43s %s|%n", " Nama       | " + barangTerbawah.nama, "");
+            System.out.printf("|%-43s %s|%n", " Kategori   | " + barangTerbawah.kategori, "");
+            System.out.println("==============================================");
+            return barangTerbawah;
+        } else {
+            System.out.println("         !!! TUMPUKAN BARANG KOSONG !!!       ");
+            System.out.println("==============================================");
+            return null;
+        }
+
+    }
+
+    public void FindSeqKode(int cari) {
+        int posisi = -1;
+        for (int i = 0; i < tumpukan.length; i++) {
+            if (tumpukan[i] != null && tumpukan[i].kode == cari) {
+                posisi = i;
+                break;
+            }
+        }
+        Tampil(posisi);
+    }
+
+    public void FindSeqNama(String cari) {
+        int posisi = -1;
+        for (int i = 0; i < tumpukan.length; i++) {
+            if (tumpukan[i] != null && tumpukan[i].nama.equalsIgnoreCase(cari)) {
+                posisi = i;
+                break;
+            }
+        }
+        Tampil(posisi);
+    }
+
+    public void Tampil(int posisi){
+        System.out.println("==============================================");
+        System.out.println("|               PENCARIAN BARANG             |");
+        System.out.println("----------------------------------------------");
+        if (posisi != -1) {
+            // Barang12 cariBarang = tumpukan[posisi];
+            System.out.printf("|%-43s %s|%n", " Kode       | " + tumpukan[posisi].kode, "");
+            System.out.printf("|%-43s %s|%n", " Nama       | " + tumpukan[posisi].nama, "");
+            System.out.printf("|%-43s %s|%n", " Kategori   | " + tumpukan[posisi].kategori, "");
+            System.out.println("==============================================");
+        } else {
+            System.out.println("         !!! BARANG TIDAK DITEMUKAN !!!       ");
+            System.out.println("==============================================");
+            
+        }
+    }
+    
+    void insertionSortKode() {
+        for (int i = 1; i < tumpukan.length; i++) {
+            Barang12 temp = tumpukan[i];
+            int j = i;
+            while (j > 0 && tumpukan[j - 1].kode - temp.kode > 0) {
+                tumpukan[j] = tumpukan[j - 1];
+                j--;
+            }
+            tumpukan[j] = temp;
+        }
+    }
+    
+    public int FindBinarySearchKode(int cari, int left, int right) {
+        insertionSortKode();
+        int mid;
+        if (right >= left) {
+            mid = (left + right) / 2;
+            if (cari == tumpukan[mid].kode) {
+                return mid;
+            } else if (tumpukan[mid].kode - cari > 0) {
+                return FindBinarySearchKode(cari, left, mid - 1);
+            } else {
+                return FindBinarySearchKode(cari, mid + 1, right);
+            }
+        }
+        return -1;
+    }
+    void insertionSortNama() {
+        for (int i = 1; i < tumpukan.length; i++) {
+            Barang12 temp = tumpukan[i];
+            int j = i;
+            while (j > 0 && tumpukan[j - 1].kode - temp.kode > 0) {
+                tumpukan[j] = tumpukan[j - 1];
+                j--;
+            }
+            tumpukan[j] = temp;
+        }
+    }
+    
+    public int FindBinarySearchNama(String cari, int left, int right) {
+        insertionSortNama();
+        int mid;
+        if (right >= left) {
+            mid = (left + right) / 2;
+            if (cari.equalsIgnoreCase(tumpukan[mid].nama)) {
+                return mid;
+            } else if (tumpukan[mid].nama.compareTo(cari) < 0) {
+                return FindBinarySearchNama(cari, left, mid - 1);
+            } else {
+                return FindBinarySearchNama(cari, mid + 1, right);
+            }
+        }
+        return -1;
     }
 }
--- Pertemuan08/Percobaan/Percobaan1_2/Utama12.java
+++ Pertemuan08/Percobaan/Percobaan1_2/Utama12.java
@@ -5,24 +5,29 @@ import java.util.Scanner;
 public class Utama12 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Gudang12 gudang = new Gudang12(7);
-        int jmlData = 7;
+        Scanner scs = new Scanner(System.in);
+        // Gudang12 gudang = new Gudang12(7);
         boolean stop = true;
-        int posisi;
 
         System.out.println("----------------------------------------------");
         System.out.println("|      SELAMAT DATANG DI GUDANG LILISYA      |");
         System.out.println("----------------------------------------------");
-
-        while (true) {
+        System.out.print("| Masukkan jumlah tumpukan: ");
+        int jmlData = scs.nextInt();
+        Gudang12 gudang = new Gudang12(jmlData);
+        while (stop) {
             System.out.println("==============================================");
             System.out.println("| Menu Utama:                                |");
             System.out.println("| 1. Tambah Barang                           |");
             System.out.println("| 2. Ambil Barang                            |");
             System.out.println("| 3. Tampilkan Tumpukan Barang               |");
-            System.out.println("| 4. Keluar                                  |");
+            System.out.println("| 4. Lihat Barang Teratas                    |");
+            System.out.println("| 5. Lihat Barang Terbawah                   |");
+            System.out.println("| 6. Cari Barang                             |");
+            System.out.println("| 0. Keluar                                  |");
             System.out.println("----------------------------------------------");
-            System.out.print("| Pilih Menu (1/2/3/4) : ");
+            // System.out.print("| Pilih Menu (1/2/3/0) : ");
+            System.out.print("| Pilih Menu (1/2/3/4/5/6/0) : ");
             int pilihan = sc.nextInt();
             sc.nextLine();
 
@@ -32,8 +37,8 @@ public class Utama12 {
                     System.out.println("|             TAMBAHKAN DATA BARANG          |");
                     System.out.println("----------------------------------------------");
                     System.out.print("| Masukkan Kode Barang   : ");
-                    int kode = sc.nextInt();
-                    sc.nextLine();
+                    int kode = scs.nextInt();
+                    scs.nextLine();
                     System.out.print("| Masukkan Nama Barang   : ");
                     String nama = sc.nextLine();
                     System.out.print("| Masukkan Nama Kategori : ");
@@ -42,7 +47,6 @@ public class Utama12 {
                     Barang12 barangBaru = new Barang12(kode, nama, kategori);
                     gudang.tambahBarang(barangBaru);
 
-                    System.out.println("==============================================");
                     break;
 
                 case 2:
@@ -50,12 +54,76 @@ public class Utama12 {
                     break;
 
                 case 3:
-                    System.out.println("==============================================");
                     gudang.tampilkanBarang();
                     break;
 
                 case 4:
+                    gudang.lihatBarangTeratas();
+                    break;
 
+                case 5:
+                    gudang.lihatBarangTerbawah();
+                    break;
+
+                case 6:
+                    boolean stopp = true;
+                    while (stopp) {
+
+                        System.out.println("==============================================");
+                        System.out.println("|               PENCARIAN BARANG             |");
+                        System.out.println("----------------------------------------------");
+                        System.out.println("| Pilih :                                    |");
+                        System.out.println("| 1. Berdasarkan Kode Barang                 |");
+                        System.out.println("| 2. Berdasarkan Nama Barang                 |");
+                        System.out.println("| 0. Kembali                                 |");
+                        System.out.println("----------------------------------------------");
+                        System.out.print("| Pilih Menu (1/2/0) : ");
+                        int pilih = scs.nextInt();
+                        scs.nextLine();
+                        switch (pilih) {
+                            case 1:
+                                System.out.println("==============================================");
+                                System.out.println("|               CARI KODE BARANG             |");
+                                System.out.println("----------------------------------------------");
+                                System.out.print("| Masukkan Kode Barang  : ");
+                                int cariKode = scs.nextInt();
+                                scs.nextLine();
+                                // gudang.FindSeqKode(cariKode);
+                                int posisiKode = gudang.FindBinarySearchKode(cariKode, 0, gudang.top);
+                                gudang.Tampil(posisiKode);
+                                break;
+
+                            case 2:
+                                System.out.println("==============================================");
+                                System.out.println("|               CARI NAMA BARANG             |");
+                                System.out.println("----------------------------------------------");
+                                System.out.print("| Masukkan Kode Barang  : ");
+                                String cariNama = sc.nextLine();
+                                // gudang.FindSeqNama(cariNama);
+                                int posisiNama = gudang.FindBinarySearchNama(cariNama, 0, gudang.top);
+                                gudang.Tampil(posisiNama);
+                                break;
+
+                            case 0:
+                                System.out.println("----------------------------------------------");
+                                System.out.println("|         !! KEMBALI KE MENU UTAMA !!        |");
+                                System.out.println("----------------------------------------------");
+
+                                stopp = false;
+                                break;
+
+                            default:
+                                break;
+                        }
+                    }
+                    break;
+
+                case 0:
+                    System.out.println("----------------------------------------------");
+                    System.out.println("|         !!! KELUAR DARI PROGRAM !!!        |");
+                    System.out.println("----------------------------------------------");
+
+                    stop = false;
                     break;
 
                 default:
