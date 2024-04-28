cda63c7ec50e30f1f3ec8d4e2c109fa7923227e8
Ghetsa <ghetsa.arryanti@gmail.com>
2024-04-28 21:03:50 +0700

    update: fix typo on postfixmain

--- Pertemuan08/Percobaan/Percobaan3/PostfixMain12.java
+++ Pertemuan08/Percobaan/Percobaan3/PostfixMain12.java
@@ -5,13 +5,17 @@ public class PostfixMain12 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String P, Q;
-        System.out.println("Masukkan ekspresi matematika (infix) : ");
+        System.out.println("======================================================");
+        System.out.print("| Masukkan ekspresi matematika (infix): ");
         Q = sc.nextLine();
         Q = Q.trim();
         Q = Q + ")";
         int total = Q.length();
         Postfix12 post = new Postfix12(total);
         P = post.konversi(Q);
-        System.out.println("Postfix: " + P);
+        System.out.println("------------------------------------------------------");
+        System.out.printf("|%-50s %s|%n", "Postfix: " + P, "");
+        System.out.print("======================================================");
+
     }
 }
