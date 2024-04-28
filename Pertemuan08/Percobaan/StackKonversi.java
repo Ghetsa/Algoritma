e6e90a3ce38a0e1067de53e8c00484ea7bb8ad40
Ghetsa <ghetsa.arryanti@gmail.com>
2024-04-28 14:05:03 +0700

    add: percobaan2

---
+++ Pertemuan08/Percobaan/StackKonversi.java
@@ -0,0 +1,44 @@
+package Algoritma.Pertemuan08.Percobaan;
+
+
+public class StackKonversi {
+    int[] tumpukanBiner;
+    int size;
+    int top;
+
+    public StackKonversi() {
+        this.size = 32;
+        tumpukanBiner = new int[size];
+        top = -1;
+
+    }
+
+    public boolean isEmpty() {
+        return top == -1;
+    }
+
+    public boolean isFull() {
+        return top == size - 1;
+    }
+
+    public void push(int data) {
+        if (isFull()) {
+            System.out.println("Stack Penuh");
+        } else {
+            top++;
+            tumpukanBiner[top] = data;
+        }
+    }
+
+    public int pop() {
+        if (isEmpty()) {
+            System.out.println("Stack Kosong!!!");
+            return -1;
+        } else {
+            int data = tumpukanBiner[top];
+            top--;
+            return data;
+        }
+    }
+
+}
